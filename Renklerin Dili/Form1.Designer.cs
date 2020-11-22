c55a8b356e61b22dc694f003e08b16b8dacd1b00
Eymen Uraş <eymenuras123@gmail.com>
2020-11-22 20:22:52 +0300

    11

---
+++ Renklerin Dili/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Renklerin_Dili
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kırmızı",
+            "Beyaz",
+            "Yeşil",
+            "Kahverengi",
+            "Siyah",
+            "Gri",
+            "Mor",
+            "Mavi",
+            "Sarı",
+            "Turuncu",
+            "Pembe"});
+            this.comboBox1.Location = new System.Drawing.Point(124, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Renk Seçiniz:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(16, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tamam";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "00";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 28);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 158);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Renklerin Dili";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Renklerin Dili/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Renklerin_Dili
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Kırmızı") 
+            {
+                label2.Text = "Dikkat Çekici Bir Renktir.";
+                button2.BackColor = Color.Red;
+            }
+            if(comboBox1.Text == "Beyaz")
+            {
+                label2.Text = "Temizliği ve Saflığı İfade Eder.";
+                button2.BackColor = Color.White;
+            }
+            if (comboBox1.Text == "Yeşil") 
+            {
+                label2.Text = "Vejeteryanlığı Temsil Eder.";
+                button2.BackColor = Color.Green;
+            }
+            if (comboBox1.Text == "Kahverengi") 
+            {
+                label2.Text = "Kahverengi İnsanların Daha Hızlı Hareket Etmelerine Neden Olur.";
+                button2.BackColor = Color.Brown;
+            }
+            if (comboBox1.Text == "Siyah") 
+            {
+                label2.Text = "Hırsı ve Gücü Temsil Eder.";
+                button2.BackColor = Color.Black;
+            }
+            if (comboBox1.Text == "Gri") 
+            {
+                label2.Text = "Diplomatik ve Ağır bir Renktir.";
+                button2.BackColor = Color.Gray;
+            }
+            if (comboBox1.Text == "Mor") 
+            {
+                label2.Text = "Mor Renginin İnsanların Bilinç Altlarındaki Korkuyu Açığa Çıkarttığı Tespit Edilmiştir.";
+                button2.BackColor = Color.Purple;
+            }
+            if (comboBox1.Text == "Mavi") 
+            {
+                label2.Text = "Sakinleştirici bir Renktir.";
+                button2.BackColor = Color.Blue;
+            }
+            if (comboBox1.Text == "Sarı") 
+            {
+                label2.Text = "Geçiciliğin İfadesidir.";
+                button2.BackColor = Color.Yellow;
+            }
+            if (comboBox1.Text == "Turuncu") 
+            {
+                label2.Text = "Kırmızı Gibi Dikkat Çekici bir Renktir.";
+                button2.BackColor = Color.Orange;
+            }
+            if (comboBox1.Text == "Pembe")
+            { 
+                label2.Text = "Karşı Tarafa Rahatlık Hissi Gönderir.";
+                button2.BackColor = Color.Pink;
+            }
+        }
+    }
+}
